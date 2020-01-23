645cebce5b8f11d23220b9cd6db52382b4b50e31
Pam Cairo <pcairo_90@outlook.com>
2020-01-23 00:03:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,3 +9,14 @@ class Anagram
   def match(array_of_words)
     
     
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
+  
+  
+end
+
+listen = Anagram.new("listen")
+#listen.match(%w(enlists))
+listen.match(%w(enlists google inlets banana))
